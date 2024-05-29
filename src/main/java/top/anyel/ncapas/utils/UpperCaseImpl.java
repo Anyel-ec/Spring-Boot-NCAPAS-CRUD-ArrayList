49aedf1e6d0c0e8cfc2645dfb0655fb62eecdb0b
anyel-ec <paulangel826@gmail.com>
2024-05-29 17:24:02 -0500

    add files to logging

--- src/main/java/top/anyel/ncapas/utils/UpperCaseImpl.java
+++ src/main/java/top/anyel/ncapas/utils/UpperCaseImpl.java
@@ -3,7 +3,7 @@ package top.anyel.ncapas.utils;
 public class UpperCaseImpl implements UpperCase {
     public String upperCase(String texto){
         if (texto == null) {
-            return null;
+            return "";
         }
         return texto.toUpperCase();
     }
